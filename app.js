827414dbb1b0dc69b5db6392f9807e800105da3d
Safae Mliha-Touati <57368349+SafaeMT@users.noreply.github.com>
2020-06-18 11:32:57 +0200

    implement module pattern & dependency injection to structure the code

--- app.js
+++ app.js
@@ -1,148 +1,167 @@
-let screen = document.querySelector('.screen');
+const myCalculatorAPI = createCalculator();
 
-const MAX_DISPLAY_LENGTH = 14;
-let firstArgument = 0;
-let secondArgument;
-let operator;
-let screenValue = 0;
+myCalculatorAPI.init({
+    screenSelector: '.screen',
+    buttonsWrapperSelector: '.buttons'
+});
 
-// Utilisation d'un seul event listener qui tire profit de la propagation des évènements
-document.querySelector('.buttons').addEventListener('click', handleButtonClick);
+// ********** DEFINITIONS **********
+function createCalculator() {
+    let screen;
 
-function handleButtonClick(e) {
-    if (e.target.tagName === 'BUTTON') {
-        switch (e.target.innerText) {
-            case 'C':
-                reset();
-                break;
+    const MAX_DISPLAY_LENGTH = 14;
+    let firstArgument = 0;
+    let secondArgument;
+    let operator;
+    let screenValue = 0;
 
-            case '←':
-                deleteLastDigit();
-                break;
+    let api = { init };
+    return api;
 
-            case '=':
-                calculateResult();
-                break;
+    function init({ screenSelector, buttonsWrapperSelector }) {
+        screen = document.querySelector(screenSelector);
+        render();
 
+        // Utilisation d'un seul event listener qui tire profit de la propagation des évènements
+        document.querySelector(buttonsWrapperSelector).addEventListener('click', handleButtonClick);
+    }
+
+    // ********** PRIVATE FUNCTIONS **********
+    function handleButtonClick(e) {
+        if (e.target.tagName === 'BUTTON') {
+            switch (e.target.innerText) {
+                case 'C':
+                    reset();
+                    break;
+
+                case '←':
+                    deleteLastDigit();
+                    break;
+
+                case '=':
+                    calculateResult();
+                    break;
+
+                case '+':
+                case '−':
+                case '×':
+                case '÷':
+                    updateState(e.target.innerText);
+                    break;
+
+                default:
+                    addDigit(e.target.innerText);
+                    break;
+            }
+
+            render();
+        }
+    }
+
+    function reset() {
+        firstArgument = 0;
+        secondArgument = operator = undefined;
+
+        updateScreenValue();
+    }
+
+    function deleteLastDigit() {
+        if (operator == undefined) {
+            firstArgument = Number(String(firstArgument).slice(0, -1));
+        } else {
+            secondArgument = Number(String(secondArgument).slice(0, -1));
+        }
+
+        // Réinitialisation de l'opérateur si le second argument = undefined
+        // et que le nombre affiché à l'écran est modifié
+        if (secondArgument == undefined) {
+            operator = undefined;
+        }
+
+        updateScreenValue();
+    }
+
+    function updateState(operatorText) {
+        calculateResult();
+        operator = operatorText;
+    }
+
+    function addDigit(digitText) {
+        if (operator == undefined) {
+            firstArgument = Number(firstArgument + digitText);
+        } else {
+            secondArgument = Number(secondArgument == undefined ? digitText : secondArgument + digitText);
+        }
+
+        updateScreenValue();
+    }
+
+    function add(x, y) {
+        return x + y;
+    }
+
+    function substract(x, y) {
+        return x - y;
+    }
+
+    function multiply(x, y) {
+        return x * y;
+    }
+
+    function divide(x, y) {
+        return x / y;
+    }
+
+    function calculateResult() {
+        if (secondArgument == undefined) {
+            return;
+        }
+
+        switch (operator) {
             case '+':
-            case '−':
-            case '×':
-            case '÷':
-                updateState(e.target.innerText);
+                firstArgument = add(firstArgument, secondArgument);
                 break;
 
-            default:
-                addDigit(e.target.innerText);
+            case '−':
+                firstArgument = substract(firstArgument, secondArgument);
+                break;
+
+            case '×':
+                firstArgument = multiply(firstArgument, secondArgument);
+                break;
+
+            case '÷':
+                firstArgument = divide(firstArgument, secondArgument);
+                if (!Number.isInteger(firstArgument)) {
+                    firstArgument = formatDecimalNumber(firstArgument);
+                }
                 break;
         }
 
-        render();
-    }
-}
-
-function reset() {
-    firstArgument = 0;
-    secondArgument = operator = undefined;
-
-    updateScreenValue();
-}
-
-function deleteLastDigit() {
-    if (operator == undefined) {
-        firstArgument = Number(String(firstArgument).slice(0, -1));
-    } else {
-        secondArgument = Number(String(secondArgument).slice(0, -1));
+        secondArgument = operator = undefined;
+        updateScreenValue();
     }
 
-    // Réinitialisation de l'opérateur si le second argument = undefined
-    // et que le nombre affiché à l'écran est modifié
-    if (secondArgument == undefined) {
-        operator = undefined;
+    function formatDecimalNumber(decimalNumber) {
+        const decimalNumberPartsArr = String(decimalNumber).split('.'); // [integerPart, decimalPart]
+        let availableLength = MAX_DISPLAY_LENGTH - decimalNumberPartsArr[0].length;
+
+        if (decimalNumberPartsArr[1].length <= availableLength) {
+            return decimalNumber;
+        }
+
+        return decimalNumber.toFixed(availableLength);
     }
 
-    updateScreenValue();
-}
-
-function updateState(operatorText) {
-    calculateResult();
-    operator = operatorText;
-}
-
-function addDigit(digitText) {
-    if (operator == undefined) {
-        firstArgument = Number(firstArgument + digitText);
-    } else {
-        secondArgument = Number(secondArgument == undefined ? digitText : secondArgument + digitText);
+    function updateScreenValue() {
+        if (secondArgument == undefined) {
+            screenValue = firstArgument;
+        } else {
+            screenValue = secondArgument;
+        }
     }
 
-    updateScreenValue();
-}
-
-function add(x, y) {
-    return x + y;
-}
-
-function substract(x, y) {
-    return x - y;
-}
-
-function multiply(x, y) {
-    return x * y;
-}
-
-function divide(x, y) {
-    return x / y;
-}
-
-function calculateResult() {
-    if (secondArgument == undefined) {
-        return;
+    function render() {
+        screen.innerText = screenValue;
     }
-
-    switch (operator) {
-        case '+':
-            firstArgument = add(firstArgument, secondArgument);
-            break;
-
-        case '−':
-            firstArgument = substract(firstArgument, secondArgument);
-            break;
-
-        case '×':
-            firstArgument = multiply(firstArgument, secondArgument);
-            break;
-
-        case '÷':
-            firstArgument = divide(firstArgument, secondArgument);
-            if (!Number.isInteger(firstArgument)) {
-                firstArgument = formatDecimalNumber(firstArgument);
-            }
-            break;
-    }
-
-    secondArgument = operator = undefined;
-    updateScreenValue();
-}
-
-function formatDecimalNumber(decimalNumber) {
-    const decimalNumberPartsArr = String(decimalNumber).split('.'); // [integerPart, decimalPart]
-    let availableLength = MAX_DISPLAY_LENGTH - decimalNumberPartsArr[0].length;
-
-    if (decimalNumberPartsArr[1].length <= availableLength) {
-        return decimalNumber;
-    }
-
-    return decimalNumber.toFixed(availableLength);
-}
-
-function updateScreenValue() {
-    if (secondArgument == undefined) {
-        screenValue = firstArgument;
-    } else {
-        screenValue = secondArgument;
-    }
-}
-
-function render() {
-    screen.innerText = screenValue;
 }
